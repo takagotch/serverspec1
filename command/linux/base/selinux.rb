dee59b6d1ab9d8d7d53de7d0228183a452fe8e6d
takagotch <dyaccb@gmail.com>
2018-01-21 03:18:19 +0900

    check_has_mode(mode)

---
+++ command/linux/base/selinux.rb
@@ -0,0 +1,16 @@
+#class Specinfra::Command::Base::Selinux < Spicinfra::Command::Base
+#end
+#
+class Specinfra::Command::Linux::Base::Selinux < Specinfra::Command::Base::Selinux
+  class << self
+    def check_has_mode(mode)
+      cmd = ""
+      cmd += "test ! -f /etc/selinux/config || (" if mode == "disabled"
+      cmd += "getenforce | grep -i -- #{escape(mode)} "
+      cmd += "&& grep -i -- ^SELINUX=#{escape(mode)}$ /etc/selinux/config"
+      cmd += ")" if mode == "disaled"
+      cmd
+    end
+  end
+end
+
